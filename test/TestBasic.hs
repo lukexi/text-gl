a1f926b33261fbb2a125c8479bf60bb92a75138f
Luke Iannini <lukexi@me.com>
2015-11-09 01:28:45 -0800

    Simplify basic test. Remove extraneous files. Add TODO

--- test/TestBasic.hs
+++ test/TestBasic.hs
@@ -1,70 +1,54 @@
 {-# LANGUAGE RecordWildCards #-}
-import qualified Graphics.UI.GLFW as GLFW
-import Graphics.GL
+import Graphics.UI.GLFW.Pal
 import Graphics.GL.Pal
 import Graphics.GL.Freetype
 
 import Control.Monad
 import System.Random
-import Linear
-
-import SetupGLFW
 
 -------------------------------------------------------------
 -- A test to make sure font rendering works
 -------------------------------------------------------------
 
-resX, resY :: Num a => a
-resX = 1920
-resY = 1080
-
-
-
 main :: IO a
 main = do
 
-    win <- setupGLFW "Freetype-GL" resX resY
+    (win, events) <- createWindow "Freetype-GL" 1024 768
 
     glyphQuadProg <- createShaderProgram "test/glyphQuad.vert" "test/glyphQuad.frag"
     font          <- makeGlyphs "freetype-gl/fonts/Vera.ttf" 50 glyphQuadProg
 
     glClearColor 0 0.1 0.1 1
-    glEnable GL_DEPTH_TEST
-
-    forever $ 
-        mainLoop win font 
-
-
-mainLoop :: GLFW.Window -> Font -> IO ()
-mainLoop win font = do
-    -- glGetErrors
-
-    -- Get mouse/keyboard/OS events from GLFW
-    GLFW.pollEvents
-
-    -- Clear the framebuffer
-    glClear (GL_COLOR_BUFFER_BIT .|. GL_DEPTH_BUFFER_BIT)
+    -- glEnable GL_DEPTH_TEST
     glEnable GL_BLEND
     glBlendFunc GL_SRC_ALPHA GL_ONE_MINUS_SRC_ALPHA
 
-    let xOffset = (-1)
-        -- (-xOffset/2)
+    forever $ 
+        mainLoop win events font 
 
-    -- Render our scene
-    let projection = perspective 45 (resX/resY) 0.01 1000
-        model      = mkTransformation 1 (V3 xOffset 0 (-4))
-        view       = lookAt (V3 0 2 500) (V3 0 0 (-4)) (V3 0 1 0)
-        (x,y,w,h)  = (0,0,1920,1080)
 
+mainLoop :: Window -> Events -> Font -> IO ()
+mainLoop win events font = do
+    -- Get mouse/keyboard/OS events from GLFW
+    processEvents events $ closeOnEscape win
+
+    -- Update the viewport and projection
+    (x,y,w,h) <- getWindowViewport win
     glViewport x y w h
+    projection44 <- getWindowProjection win 45 0.01 1000
 
-    uniformM44 (uViewProjection (fgUniforms font)) (projection !*! view)
+    -- Clear the framebuffer
+    glClear (GL_COLOR_BUFFER_BIT .|. GL_DEPTH_BUFFER_BIT)
 
-    -- renderCube cube mvp
+    -- Create our model view projection matrix
+    let model44      = mkTransformation 1 (V3 (-1) 0 (-4))
+                        !*! scaleMatrix 0.005
+        view44       = lookAt (V3 0 2 0) (V3 0 0 (-4)) (V3 0 1 0)
+        mvp          = projection44 !*! view44 !*! model44
+
+    -- Render random characters
     frameChars <- replicateM 10 $ randomRIO (' ','~')
-    _ <- renderText font frameChars model
+    renderText font frameChars (-1,3) mvp
     
-    GLFW.swapBuffers win
-
-
+    swapBuffers win
 
--- test/TestBuffer.hs
+++ test/TestBuffer.hs
@@ -15,10 +15,6 @@ import Halive.Utils
 
 import TextBuffer
 
-resX, resY :: Num a => a
-resX = 1024
-resY = 768
-
 -- data AppState = AppState 
 --     { _appTextBuffer :: Buffer
 --     }
@@ -36,7 +32,7 @@ fontFile = "freetype-gl/fonts/SourceCodePro-Regular.ttf"
 main :: IO ()
 main = do
 
-    (win, events) <- reacquire 0 $ createWindow "Tiny Rick" resX resY
+    (win, events) <- reacquire 0 $ createWindow "Tiny Rick" 1024 768
 
     glyphQuadProg <- createShaderProgram "test/glyphQuad.vert" "test/glyphQuad.frag"
     font          <- makeGlyphs fontFile 30 glyphQuadProg
@@ -44,6 +40,9 @@ main = do
     glClearColor 0 0.1 0.1 1
     glEnable GL_DEPTH_TEST
     glDisable GL_DEPTH_TEST
+
+    glEnable    GL_BLEND
+    glBlendFunc GL_SRC_ALPHA GL_ONE_MINUS_SRC_ALPHA
 
     text <- readFile "test/TestBuffer.hs"
     -- let initialState = newAppState { _appTextBuffer = bufferFromString text }
@@ -72,32 +71,28 @@ mainLoop win events font = do
                     bufferString <- gets stringFromBuffer
                     liftIO $ writeFile "test/TestBuffer.hs" bufferString
             | shiftIsDown -> do
-                onKeyDown Key'Left e $ selectLeft
+                onKeyDown Key'Left  e $ selectLeft
                 onKeyDown Key'Right e $ selectRight
             | otherwise -> do
                 case e of
                     Character char -> insertChar char
                     _ -> return ()
-                onKeyDown Key'Backspace e $ 
-                    backspace
-                onKeyDown Key'Enter e $ do
-                    insertChar '\n'
-                onKeyDown Key'Left e $ moveLeft
-                onKeyDown Key'Right e $ moveRight
-                onKeyDown Key'Down e $ moveDown
-                onKeyDown Key'Up e $ moveUp
+                onKeyDown Key'Backspace e $ backspace
+                onKeyDown Key'Enter     e $ insertChar '\n'
+                onKeyDown Key'Left      e $ moveLeft
+                onKeyDown Key'Right     e $ moveRight
+                onKeyDown Key'Down      e $ moveDown
+                onKeyDown Key'Up        e $ moveUp
 
     immutably $ do
         -- Clear the framebuffer
         glClear (GL_COLOR_BUFFER_BIT .|. GL_DEPTH_BUFFER_BIT)
-        glEnable GL_BLEND
-        glBlendFunc GL_SRC_ALPHA GL_ONE_MINUS_SRC_ALPHA
 
         -- Render our scene
         let view44       = viewMatrixFromPose newPose
             model44      = mkTransformation (axisAngle (V3 0 1 0) 0) (V3 (-2) (2) (-4))
                                 !*! scaleMatrix 0.003
-            mvp = projection44 !*! view44 !*! model44
+            mvp          = projection44 !*! view44 !*! model44
 
         buffer <- get
         renderText font (bufText buffer) (bufSelection buffer) mvp
